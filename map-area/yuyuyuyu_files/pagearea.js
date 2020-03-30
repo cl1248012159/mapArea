f2f140a0f42584584f5c5f7b7c5f37e9701737b2
amy chen <313399524@qq.com>
2020-03-30 13:33:21 +0800

    工具

---
+++ map-area/yuyuyuyu_files/pagearea.js
@@ -0,0 +1,648 @@
+var _$ = ["#copycode", "",
+ ".sameh",
+ "#editArea img",
+ "#copycode",
+ "#preview .areaContent",
+ "",
+ "",
+ ".resizable",
+ ".closeBtn",
+ "tabindex",
+ ".resizable",
+ ".closeBtn",
+ "display",
+ "block",
+ "#editArea .areaContent",
+ "mousedown",
+ "title",
+ "双击区域添加链接",
+ ".closeBtn",
+ "display",
+ "none",
+ "title",
+ "",
+ ".resizable",
+ "parent",
+ "n, e, s, w, ne, se, sw, nw",
+ "#preview area",
+ "coords",
+ ",",
+ ",",
+ ",",
+ ".resizable",
+ "selected",
+ "selected",
+ ".draggable",
+ ".areaContent",
+ "#preview area",
+ "coords",
+ ",",
+ ",",
+ ",",
+ ".resizable",
+ "selected",
+ "selected",
+ ".resizable",
+ ".resizable",
+ "selected",
+ "selected",
+ ".closeBtn",
+ "click",
+ ".closeBtn",
+ "click",
+ "确定要删除吗？",
+ "#preview area",
+ "#editArea .areaContent",
+ "mousedown",
+ ".areaContent",
+ ".areaContent",
+ "#editArea .areaContent",
+ "<div class='ui-widget-content resizable draggable' style='left:",
+ "px;top:",
+ "px;width:0px;height:0px'><div class='ui-hline'></div><div class='ui-hline bottom'></div><div class='ui-vline right'></div><div class='ui-vline'></div><div class='ui-button-icon-default ui-icon ui-icon-closethick closeBtn' title='删除区域' tabindex=''></div></div>",
+ "#preview map",
+ "<area target='_blank' shape='rect'>",
+ ".areaContent",
+ ".areaContent",
+ ".resizable",
+ "#preview area",
+ ",",
+ ",",
+ ",",
+ "",
+ "#",
+ "mousemove",
+ ".resizable",
+ ".resizable",
+ ".resizable",
+ "#preview area",
+ "mousemove",
+ "cursor",
+ "crosshair",
+ "#editArea .areaContent",
+ "background-image",
+ "#areaAdd",
+ "#editArea .areaContent",
+ "<div class='ui-widget-content resizable draggable' style='left:25px;top:0px;width:100px;height:100px'><div class='ui-hline'></div><div class='ui-hline bottom'></div><div class='ui-vline right'></div><div class='ui-vline'></div><div class='ui-button-icon-default ui-icon ui-icon-closethick closeBtn' title='删除区域' tabindex=''></div></div>",
+ "#preview map",
+ "<area target='_blank' shape='rect'>",
+ ".resizable",
+ "dblclick",
+ "#dialogArea",
+ "open",
+ ".ui-dialog-title",
+ "区域",
+ "设置",
+ ".ui-dialog-title",
+ "tabindex",
+ "#edit-link",
+ "#preview area",
+ "href",
+ "#edit-title",
+ "#preview area",
+ "title",
+ "#edit-left",
+ "#edit-top",
+ "#edit-width",
+ "#edit-height",
+ "#preview area",
+ "target",
+ "undefined",
+ "#edit-target",
+ "checked",
+ "#edit-target",
+ "checked",
+ "#areaPic",
+ "#dialogPic",
+ "open",
+ "#editPic",
+ "#editArea .areaContent",
+ "background-image",
+ "#editArea .areaContent",
+ "title",
+ "拖动鼠标增加热区",
+ ".resizable",
+ "#preview map",
+ "<area target='_blank' shape='rect'>",
+ ".resizable",
+ "#preview img",
+ "src",
+ "#editArea .areaContent",
+ "background-image",
+ "#preview area",
+ ",",
+ ",",
+ ",",
+ "鼠标经过",
+ "显示内容",
+ "https://mofeiyu.taobao.com",
+ ".closeBtn",
+ "tabindex",
+ "#btn-preview, #btn-preview-close",
+ "#btn-preview",
+ "#btn-preview-close",
+ "#preview",
+ "#editArea",
+ "#editWidth",
+ ".areaContent",
+ "#editWidth",
+ "#editWidth",
+ "#preview #codeArea",
+ "#editWidth",
+ ".resizable",
+ ".resizable",
+ "selected",
+ "selected",
+ "#dialogArea",
+ "scale",
+ "scale",
+ "保存",
+ ".ui-dialog-title",
+ "tabindex",
+ "#edit-left",
+ "#edit-top",
+ "#edit-left",
+ "#edit-width",
+ "#edit-top",
+ "#edit-height",
+ "#preview area",
+ "#edit-link",
+ "#edit-title",
+ ",",
+ ",",
+ ",",
+ ".resizable",
+ "#edit-left",
+ "px",
+ "#edit-top",
+ "px",
+ "#edit-width",
+ "#edit-height",
+ "#edit-target",
+ "checked",
+ "#preview area",
+ "target",
+ "_blank",
+ "#preview area",
+ "target",
+ "close",
+ "删除热区",
+ ".ui-dialog-title",
+ "tabindex",
+ "#preview area",
+ ".resizable",
+ "close",
+ "center",
+ "center",
+ "#editArea",
+ "关闭窗口",
+ "#editPic",
+ "focus",
+ "#editArea img",
+ "src",
+ "",
+ "#editArea .areaContent",
+ "background-image",
+ "url(\"",
+ "#editPic",
+ "\")",
+ "#editArea img",
+ "src",
+ "#editPic",
+ "#preview img",
+ "src",
+ "#editPic",
+ ".sameh",
+ "#editArea img",
+ "focus",
+ "",
+ "#editArea img",
+ "src",
+ ".sameh",
+ "#editArea img",
+ "#dialogPic",
+ "底图图片设置",
+ "scale",
+ "scale",
+ "保存",
+ "#editArea .areaContent",
+ "background-image",
+ "url(\"",
+ "#editPic",
+ "\")",
+ "#editArea img",
+ "src",
+ "#editPic",
+ "#preview img",
+ "src",
+ "#editPic",
+ ".sameh",
+ "#editArea img",
+ "#preview img",
+ "usemap",
+ "#map",
+ "#preview map",
+ "name",
+ "map",
+ "close",
+ "关闭",
+ "close",
+ "#preview img",
+ "src",
+ "#editArea .areaContent",
+ "background-image",
+ "#addcodebtn,#addcodebtn-close",
+ "#addcodebtn,#addcodebtn-close",
+ "#areaRead",
+ "#getRead",
+ "#addcodebtn,#addcodebtn-close",
+ "#areaRead",
+ "#xmlRead",
+ "#editArea .resizable",
+ "#copyText",
+ "#editArea .areaContent",
+ "background-image",
+ "url(\"",
+ "#preview img",
+ "src",
+ "\")",
+ "#editArea img",
+ "src",
+ "#preview img",
+ "src",
+ "#editArea",
+ "#preview #codeArea",
+ "#editArea .areaContent",
+ "#preview #codeArea",
+ "#preview",
+ "#preview #codeArea",
+ ".areaContent",
+ "#preview #codeArea",
+ "#preview #codeArea",
+ ".areaContent",
+ "#preview #codeArea",
+ "#preview area",
+ "coords",
+ ",",
+ "#editArea .areaContent",
+ "<div class='ui-widget-content resizable draggable' style='left:",
+ "px;top:",
+ "px;width:",
+ "px;height:",
+ "px'><div class='ui-hline'></div><div class='ui-hline bottom'></div><div class='ui-vline right'></div><div class='ui-vline'></div><div class='ui-button-icon-default ui-icon ui-icon-closethick closeBtn' title='删除区域' tabindex=''></div></div>",
+ "#makeCode",
+ "<TEXTAREA id=\"code-display\" style=\"width:800px;height:400px;\">",
+ "#preview .areaContent #map",
+ "</TEXTAREA>",
+ "代码区鼠标右键单击-全选-复制，或者按CTRL+A全选，按CTRL+C复制"];
+$.codeData = function() {
+
+    $(_$[0]).text(_$[1]);
+    $(_$[2]).height($(_$[3]).height());
+    $(_$[4]).text($.trim($(_$[5]).html()));
+
+
+
+};
+
+
+
+function wee0(wee1) {
+    return wee1.replace(/"/g, _$[6]).replace(/url\(|\)$/ig, _$[7])
+};
+$.addData = function() {
+    $(_$[8]).each(function(wee1) {
+        $(this).children(_$[9]).attr(_$[10], ($(this).index() - 0x1))
+    });
+    $(_$[11]).mouseover(function() {
+        if ($(this).width() > 0x0 || $(this).height() > 0x0) {
+            $(this).find(_$[12]).css(_$[13], _$[14])
+        };
+        $(_$[15]).unbind(_$[16]);
+        $(this).attr(_$[17], _$[18])
+    }).hover(function() {}).mouseout(function() {
+        $(this).find(_$[19]).css(_$[20], _$[21]);
+        $(this).attr(_$[22], _$[23]);
+    }).mouseleave(function() {
+        $.getArea()
+    });
+    $(_$[24]).resizable({
+
+        containment: _$[25],
+        handles: _$[26],
+        stop: function() {
+            var wee1 = [];
+            wee1[0x0] = $(this).position().left;
+            wee1[0x1] = $(this).position().top;
+            wee1[0x2] = parseInt($(this).position().left) + parseInt($(this).width());
+            wee1[0x3] = parseInt($(this).position().top) + parseInt($(this).height());
+            $(_$[27]).eq($(this).index() - 0x1).attr(_$[28], wee1[0x0] + _$[29] + wee1[0x1] + _$[30] + wee1[0x2] + _$[31] + wee1[0x3]);
+            $(_$[32]).removeClass(_$[33]);
+            $(this).addClass(_$[34]);
+            $.codeData()
+        }
+    });
+    $(_$[35]).draggable({
+        containment: _$[36],
+        stop: function() {
+            var wee1 = [];
+            wee1[0x0] = $(this).position().left;
+            wee1[0x1] = $(this).position().top;
+            wee1[0x2] = parseInt($(this).position().left) + parseInt($(this).width());
+            wee1[0x3] = parseInt($(this).position().top) + parseInt($(this).height());
+            $(_$[37]).eq($(this).index() - 0x1).attr(_$[38], wee1[0x0] + _$[39] + wee1[0x1] + _$[40] + wee1[0x2] + _$[41] + wee1[0x3]);
+            $(_$[42]).removeClass(_$[43]);
+            $(this).addClass(_$[44]);
+            $.codeData()
+        }
+    });
+    $(_$[45]).click(function() {
+        $(_$[46]).removeClass(_$[47]);
+        $(this).addClass(_$[48])
+    });
+    $(_$[49]).unbind(_$[50]);
+    $(_$[51]).bind(_$[52], function() {
+        var wee1 = $(this).parent().index() - 0x1;
+        if (confirm(_$[53])) {
+            $.getArea();
+            $(_$[54]).eq(wee1).remove();
+            $(this).parent().remove()
+        };
+        $.codeData()
+    })
+};
+$.getArea = function() {
+    $(_$[55]).unbind(_$[56]).mousedown(function(wee1) {
+        var wee2 = wee1.pageX - $(_$[57]).offset().left;
+        var wee3 = wee1.pageY - $(_$[58]).offset().top;
+        $(_$[59]).append(_$[60] + wee2 + _$[61] + wee3 + _$[62]);
+        $(_$[63]).append(_$[64]);
+        $(this).mousemove(function(wee4) {
+            var wee5 = [];
+            wee5[0x0] = wee4.pageX - $(_$[65]).offset().left;
+            wee5[0x1] = wee4.pageY - $(_$[66]).offset().top;
+            wee5[0x2] = wee5[0x0] - wee2;
+            wee5[0x3] = wee5[0x1] - wee3;
+            $(_$[67]).eq(-0x1).css({
+                "\x77\x69\x64\x74\x68": wee5[0x2],
+                "\x68\x65\x69\x67\x68\x74": wee5[0x3]
+            });
+            $(_$[68]).eq(-0x1).attr({
+                '\x63\x6f\x6f\x72\x64\x73': wee2 + _$[69] + wee3 + _$[70] + wee5[0x0] + _$[71] + wee5[0x1],
+                '\x74\x69\x74\x6c\x65': _$[72],
+                '\x68\x72\x65\x66': _$[73]
+            });
+            $.codeData()
+        }).mouseup(function() {
+            $(this).unbind(_$[74]);
+            if ($(_$[75]).eq(-0x1).width() < 0xa || $(_$[76]).eq(-0x1).height() < 0xa) {
+                $(_$[77]).eq(-0x1).remove();
+                $(_$[78]).eq(-0x1).remove();
+                $.codeData()
+            } else {
+                $.addData();
+            }
+        }).mouseleave(function() {
+            $(this).unbind(_$[79])
+        })
+    }).hover(function() {
+        $(this).css(_$[80], _$[81])
+    })
+};
+$(window).load(function() {
+    var wee1 = $(_$[82]).css(_$[83]);
+    var wee2 = wee1.slice(0x5, wee1.length - 0x2);
+    $(_$[84]).click(function() {
+        $(_$[85]).append(_$[86]);
+        $(_$[87]).append(_$[88]);
+        $.addData();
+        $.codeData()
+    });
+    $(_$[89]).live(_$[90], function() {
+        $(_$[91]).dialog(_$[92]);
+        $(_$[93]).text(_$[94] + $(this).index() + _$[95]);
+        $(_$[96]).attr(_$[97], $(this).index() - 0x1);
+        $(_$[98]).val($(_$[99]).eq($(this).index() - 0x1).attr(_$[100]));
+        $(_$[101]).val($(_$[102]).eq($(this).index() - 0x1).attr(_$[103]));
+        $(_$[104]).val($(this).position().left);
+        $(_$[105]).val($(this).position().top);
+        $(_$[106]).val($(this).width());
+        $(_$[107]).val($(this).height());
+        if (typeof($(_$[108]).eq($(this).index() - 0x1).attr(_$[109])) != _$[110]) {
+            $(_$[111]).prop(_$[112], true)
+        } else {
+            $(_$[113]).prop(_$[114], false)
+        }
+    });
+    $(_$[115]).click(function() {
+        $(_$[116]).dialog(_$[117]);
+        $(_$[118]).val(wee0($(_$[119]).css(_$[120])))
+    });
+    $(_$[121]).dblclick(function() {}).hover(function() {
+        $(this).attr(_$[122], _$[123])
+    });
+    for (var wee3 = 0x0; wee3 < $(_$[124]).length; wee3++) {
+        $(_$[125]).prepend(_$[126])
+    };
+    $(_$[127]).each(function(wee4) {
+        var wee5 = [];
+        wee5[0x0] = $(this).position().left;
+        wee5[0x1] = $(this).position().top;
+        wee5[0x2] = parseInt($(this).position().left) + parseInt($(this).width());
+        wee5[0x3] = parseInt($(this).position().top) + parseInt($(this).height());
+        $(_$[128]).attr(_$[129], wee0($(_$[130]).css(_$[131])));
+        $(_$[132]).eq($(this).index() - 0x1).attr({
+            '\x63\x6f\x6f\x72\x64\x73': wee5[0x0] + _$[133] + wee5[0x1] + _$[134] + wee5[0x2] + _$[135] + wee5[0x3],
+            '\x74\x69\x74\x6c\x65': _$[136] + (wee4 + 0x1) + _$[137],
+            '\x68\x72\x65\x66': _$[138]
+        });
+        $(this).children(_$[139]).attr(_$[140], ($(this).index() - 0x1))
+    });
+    $(_$[141]).click(function() {
+        $(_$[142]).toggle();
+        $(_$[143]).toggle();
+        $(_$[144]).slideToggle();
+        $(_$[145]).slideToggle();
+        $.getArea()
+    });
+    $(_$[146]).change(function() {
+        $(_$[147]).css({
+            width: $(_$[148]).val(),
+            marginLeft: -($(_$[149]).val() / 0x2)
+        });
+        $(_$[150]).width($(_$[151]).val());
+        $.codeData();
+    });
+    $(_$[152]).click(function() {
+        $(_$[153]).removeClass(_$[154]);
+        $(this).addClass(_$[155])
+    });
+    $(_$[156]).dialog({
+        width: 0x1f4,
+        height: 0xfa,
+        resizable: false,
+        autoOpen: false,
+        show: {
+            effect: _$[157]
+        },
+        hide: {
+            effect: _$[158]
+        },
+        buttons: [{
+            text: _$[159],
+            click: function() {
+                var wee4 = [];
+                var wee5 = parseInt($(_$[160]).attr(_$[161]));
+                wee4[0x0] = $(_$[162]).val();
+                wee4[0x1] = $(_$[163]).val();
+                wee4[0x2] = parseInt($(_$[164]).val()) + parseInt($(_$[165]).val());
+                wee4[0x3] = parseInt($(_$[166]).val()) + parseInt($(_$[167]).val());
+                $(_$[168]).eq(wee5).attr({
+                    '\x68\x72\x65\x66': $(_$[169]).val(),
+                    '\x74\x69\x74\x6c\x65': $(_$[170]).val(),
+                    '\x63\x6f\x6f\x72\x64\x73': wee4[0x0] + _$[171] + wee4[0x1] + _$[172] + wee4[0x2] + _$[173] + wee4[0x3]
+                });
+                $(_$[174]).eq(wee5).css({
+                    '\x6c\x65\x66\x74': $(_$[175]).val() + _$[176],
+                    '\x74\x6f\x70': $(_$[177]).val() + _$[178],
+                    '\x77\x69\x64\x74\x68': $(_$[179]).val(),
+                    '\x68\x65\x69\x67\x68\x74': $(_$[180]).val()
+                });
+                if ($(_$[181]).prop(_$[182])) {
+                    $(_$[183]).eq(wee5).attr(_$[184], _$[185])
+                } else {
+                    $(_$[186]).eq(wee5).removeAttr(_$[187])
+                };
+                $.codeData();
+                $(this).dialog(_$[188])
+            }
+        }, {
+            text: _$[189],
+            click: function() {
+                var wee4 = [];
+                var wee5 = parseInt($(_$[190]).attr(_$[191]));
+                $(_$[192]).eq(wee5).remove();
+                $(_$[193]).eq(wee5).remove();
+                $.codeData();
+                $.getArea();
+                $(this).dialog(_$[194])
+            }
+        }],
+        position: {
+            my: _$[195],
+            at: _$[196],
+            of: _$[197]
+        },
+        closeText: _$[198]
+    });
+    $(_$[199]).focus(function() {
+        $(this).addClass(_$[200]);
+        if ($(this).val() == $(_$[201]).attr(_$[202])) {
+            $(this).val(_$[203])
+        }
+    }).change(function() {
+        $(_$[204]).css(_$[205], _$[206] + $(_$[207]).val() + _$[208]);
+        $(_$[209]).attr(_$[210], $(_$[211]).val());
+        $(_$[212]).attr(_$[213], $(_$[214]).val());
+        $(_$[215]).height($(_$[216]).height())
+    }).blur(function() {
+        $(this).removeClass(_$[217]);
+        if ($(this).val() == _$[218]) {
+            $(this).val($(_$[219]).attr(_$[220]));
+            $(_$[221]).height($(_$[222]).height())
+        }
+    });
+    $(_$[223]).dialog({
+        width: 0x1f4,
+        height: 0xa0,
+        resizable: false,
+        autoOpen: false,
+        title: _$[224],
+        show: {
+            effect: _$[225]
+        },
+        hide: {
+            effect: _$[226]
+        },
+        buttons: [{
+            text: _$[227],
+            click: function() {
+                $(_$[228]).css(_$[229], _$[230] + $(_$[231]).val() + _$[232]);
+                $(_$[233]).attr(_$[234], $(_$[235]).val());
+                $(_$[236]).attr(_$[237], $(_$[238]).val());
+                $(_$[239]).height($(_$[240]).height());
+                var wee4 = (Date.parse(new Date())) / 0x3e8;
+                $(_$[241]).attr(_$[242], (_$[243] + wee4));
+                $(_$[244]).attr(_$[245], (_$[246] + wee4));
+                $(this).dialog(_$[247]);
+                $.codeData()
+            }
+        }, {
+            text: _$[248],
+            click: function() {
+                $(this).dialog(_$[249])
+            }
+        }]
+    });
+    $(_$[250]).attr(_$[251], wee0($(_$[252]).css(_$[253])));
+    $.addData();
+    $.codeData();
+    $.getArea()
+});
+$(document).ready(function() {
+    $(_$[254]).click(function() {
+        $(_$[255]).toggle();
+        $(_$[256]).slideToggle()
+    });
+    $(_$[257]).click(function() {
+        $(_$[258]).toggle();
+        $(_$[259]).slideToggle();
+        var wee1 = $(_$[260]).val();
+        $(_$[261]).remove();
+        $(_$[262]).html(wee1);
+        $(_$[263]).css(_$[264], _$[265] + $(_$[266]).attr(_$[267]) + _$[268]);
+        $(_$[269]).attr(_$[270], $(_$[271]).attr(_$[272]));
+        $(_$[273]).height($(_$[274]).height());
+        $(_$[275]).height($(_$[276]).height());
+        $(_$[277]).height($(_$[278]).height());
+        $(_$[279]).css({
+            width: $(_$[280]).width(),
+            marginLeft: -($(_$[281]).width() / 0x2)
+        });
+        $(_$[282]).width($(_$[283]).width());
+        $(_$[284]).each(function() {
+            a = $(this).attr(_$[285]).split(_$[286]);
+            $(_$[287]).append(_$[288] + a[0x0] + _$[289] + a[0x1] + _$[290] + (a[0x2] - a[0x0]) + _$[291] + (a[0x3] - a[0x1]) + _$[292]);
+            $.addData()
+        });
+    })
+});
+$(document).ready(function() {
+    $(_$[0]).html('fwefweeeeeeeeeeeeeeeeeeeee');
+
+    $(_$[293]).click(function() {
+
+        var wdth = $('#editArea').height();
+
+
+
+        $(_$[284]).each(function() {
+            $(this).css("outline", "none");
+        });
+
+        var wide = $(_$[148]).val();
+        if (wide == '1920') {
+            var wee1 = _$[294] + $.trim($(_$[295]).html().replace(/><area/g,'>\n<area')) + _$[296];
+        } else {
+            var wee1 = _$[294] + $.trim($(_$[295]).html().replace(/><area/g,'>\n<area')) + _$[296];
+        }
+
+
+        $.dialog({
+            padding: 0x0,
+            fixed: true,
+            title: _$[297],
+            content: wee1
+        })
+    })
+});
